bcadeed9ae410522e324fe905b07f18b92f05da2
Ferveloper <fermervil@gmail.com>
2019-03-07 23:31:54 +0100

    Basic home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API_KEY='687ccf3a676569dd642e0706e30a6dae'
 class App extends Component {
+
+  state = { results : [] }
+
+  async componentDidMount() {
+    const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=687ccf3a676569dd642e0706e30a6dae&language=es-ES&page=1');
+    const { results } = await response.json();
+    this.setState({ results : results})
+		console.log('TCL: App -> componentDidMount -> results', results)
+  }
+  
   render() {
+    const results = this.state.results;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <ul className='container'>
+          {results.map(movie => <li key={movie.id} className='movie'>
+          <img src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}/>
+          <div className='title'>{movie.title}</div>
+          </li>)}
+        </ul>
       </div>
     );
   }
