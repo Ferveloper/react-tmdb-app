5a70dc451a5d271e19306ebeb72aa91751363967
Ferveloper <fermervil@gmail.com>
2019-03-18 23:06:40 +0100

    Edit test collection

--- src/setStorages.js
+++ src/setStorages.js
@@ -6,5 +6,5 @@ sessionStorage.setItem('discoverPage', 1);
 sessionStorage.setItem('searchQuery', '');
 sessionStorage.setItem('searchResults', JSON.stringify([]));
 sessionStorage.setItem('searchPage', 1);
-localStorage.setItem('collections', JSON.stringify([{id : 0, name : 'Prueba 1', movies : sampleMovies}, {id : 1, name : 'Prueba 2', movies : []}]));
+localStorage.setItem('collections', JSON.stringify([{id : 0, name : 'Colección de prueba', movies : sampleMovies}]));
 localStorage.setItem('ratings', JSON.stringify(sampleRatings));
