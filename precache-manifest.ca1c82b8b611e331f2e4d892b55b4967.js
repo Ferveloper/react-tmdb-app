3c6f7c6448f8ef617e510eb0632115dd322e271c
Ferveloper <fermervil@gmail.com>
2019-10-31 09:25:15 +0100

    Update gh-pages

---
+++ precache-manifest.ca1c82b8b611e331f2e4d892b55b4967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "141521f9e79b917c0b52198536a2433c",
+    "url": "/static/media/default.141521f9.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5fda6288a53dae7b43bd",
+    "url": "/static/js/main.07ad3497.chunk.js"
+  },
+  {
+    "revision": "60e84166dd6b65f235ff",
+    "url": "/static/js/2.c3b62892.chunk.js"
+  },
+  {
+    "revision": "5fda6288a53dae7b43bd",
+    "url": "/static/css/main.ee438d53.chunk.css"
+  },
+  {
+    "revision": "fdc584fba6c7bcbc29bd59c1666e92b7",
+    "url": "/index.html"
+  }
+];
